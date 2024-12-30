965023ec72418fc99c7b614ee258daee73517ab9
Garrett Webster <garrettreeds@gmail.com>
2024-12-30 16:08:43 -0600

    Adds some comments to document functions

--- mothy.h
+++ mothy.h
@@ -3,28 +3,35 @@
 #include <functional>
 #include <iostream>
 
-void print_param_with_index(const std::vector<std::string>&, int) {
-    // This is the base case. I'm sure I could do this most elgantly, but that's a problem for future me
+// This is the base case. I'm sure I could do this most elegantly, but that's a problem for future me. Also, really
+// don't understand why it's needed, but it has something to do with the print_param_helper being called when rest
+// doesn't exist or something like that. Don't quite get it, but deleting this function makes the program
+// not compile. I tried testing sizeof...(rest), but it doesn't work
+void print_param_helper(const std::vector<std::string>&, int) {
     return;
 }
 
+// Recursively prints the parameters and passed values for each parameter
 template <typename T, typename... Args>
-void print_param_with_index(const std::vector<std::string>& param_names, int idx, T&& first, Args&&... rest) {
+void print_param_helper(const std::vector<std::string>& param_names, int idx, T&& first, Args&&... rest) {
     // Print the parameter name and value
     std::cout << param_names[idx] << ": " << first << std::endl;
 
     // Recursively call the function for the rest of the arguments
-    if constexpr (sizeof...(rest) > 0) {
-        print_param_with_index(param_names, idx + 1, std::forward<Args>(rest)...);
-    }
+    print_param_helper(param_names, idx + 1, std::forward<Args>(rest)...);
 }
 
+// Strips the object that the function was tested on and passes the remaining parameters to print_param_helper
 template <typename T, typename... Args>
-void member_func_print_param_with_index(const std::vector<std::string>& param_names, T&& first, Args&&... rest) {
-    std::cout << std::endl << "+++Parameters+++:" << std::endl;
-
-    print_param_with_index(param_names, 0, std::forward<Args>(rest)...);
+void member_func_print_param_helper(const std::vector<std::string>& param_names, T&& first, Args&&... rest) {
+    print_param_helper(param_names, 0, std::forward<Args>(rest)...);
 }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+// Because the member function test function has to be passed the object, the pack will contain one too many items. The
+// member_function_print_params uses a helper function to strip this extraneous info before passing it to the same
+// function as print_params
 
 template<typename... Args>
 void print_params(std::vector<std::string> &param_names, Args&&... args) {
@@ -35,7 +42,7 @@ void print_params(std::vector<std::string> &param_names, Args&&... args) {
     }
 
     std::cout << std::endl << "+++Parameters+++" << std::endl;
-    print_param_with_index(param_names, 0, std::forward<Args>(args)...);
+    print_param_helper(param_names, 0, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
@@ -45,13 +52,19 @@ void member_func_print_params(std::vector<std::string> &param_names, Args&&... a
         std:: cout << "param_names.size(): " << param_names.size() << std::endl << "sizeof...(args)" << sizeof...(args) << std::endl;
         return;
     }
-    member_func_print_param_with_index(param_names, std::forward<Args>(args)...);
+
+    std::cout << std::endl << "+++Parameters+++:" << std::endl;
+    member_func_print_param_helper(param_names, std::forward<Args>(args)...);
 }
+
+//---------------------------------------------------------------------------------------------------------------------
 
 
 // test() -- void
 // Checks to see if the passed function returns the expected value with the passed arguments, then
-// prints out the result of the test
+// prints out the result of the test.
+// NOTE: This function should be used to test functions that are NOT being called from an instance of a class
+//       To test a function called from a class instance, use member_func_test()
 template <typename Func, typename T, typename... Args>
 void test(Func&& func, T expected, const std::string &test_name, std::vector<std::string> param_names = {}, Args&&... args) {
     std::cout << std::endl;
@@ -85,6 +98,11 @@ void test(Func&& func, T expected, const std::string &test_name, std::vector<std
     }
 }
 
+// member_func_test() -- void
+// Checks to see if the passed function returns the expected value with the passed arguments, then
+// prints out the result of the test.
+// NOTE: This function should be used to test functions that ARE being called from an instance of a class
+//       To test a function not called from a class instance, use test()
 template <typename Func, typename T, typename... Args>
 void member_func_test(Func&& func, T expected, const std::string &test_name, std::vector<std::string> param_names = {}, Args&&... args) {
     std::cout << std::endl;
